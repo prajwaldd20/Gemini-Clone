f07c6ca3d7af16632eca62e6d096587095263aaf
Prajwal Dahake <97455383+prajwaldd20@users.noreply.github.com>
2024-06-26 17:23:19 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,4 +1,4 @@
-// const apiKey= "AIzaSyDeclglheXpmYAbmzjfirPUAftFMrc9zqk"
+// const apiKey=
 /*
  * Install the Generative AI SDK
  *
@@ -14,7 +14,7 @@ import  {
     HarmBlockThreshold,
   } from "@google/generative-ai";
   
-  const apiKey = "AIzaSyDeclglheXpmYAbmzjfirPUAftFMrc9zqk";
+  const apiKey ;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
